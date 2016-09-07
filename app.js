810a2121579484c6e221c85c2c6757b3ba0607a6
Anthony Ng <newyork.anthonyng@gmail.com>
2016-09-07 12:17:09 -0400

    Make checkDiagonals, checkHorizontals, checkVerticals private

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ $(function() {
 	console.log('app.js loaded.');
 });
 
-// board will be a 3x3 array
-/*
-[
-	[x, o, x],
-	[x, o, x],
-	[x, o, x],
-]
-Empty spaces will be null
-Filled spaces will either be 'x' or 'o'
-*/
-
 let game = (function() {
 	let gameBoard = [
 		[null, null, null],
@@ -76,9 +65,6 @@ let game = (function() {
 		getGameBoard: getGameBoard,
 		setGameBoard: setGameBoard,
 		resetGameBoard: resetGameBoard,
-		checkDiagonals: checkDiagonals,
-		checkVerticals: checkVerticals,
-		checkHorizontals: checkHorizontals,
 		checkForWinner: checkForWinner
 	};
 })();
