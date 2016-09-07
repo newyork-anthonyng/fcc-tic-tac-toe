165a4003584bd7cb3a6abcc2ed035d494a813b5b
Anthony Ng <newyork.anthonyng@gmail.com>
2016-09-07 10:53:45 -0400

    Add getGameBoard method with passing tests

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+'use strict';
+
 $(function() {
 	console.log('app.js loaded.');
 });
+
+// board will be a 3x3 array
+/*
+[
+	[x, o, x],
+	[x, o, x],
+	[x, o, x],
+]
+Empty spaces will be null
+Filled spaces will either be 'x' or 'o'
+*/
+
+let game = (function() {
+	let gameBoard = [
+		[null, null, null],
+		[null, null, null],
+		[null, null, null]
+	];
+
+	let getGameBoard = function() {
+		return gameBoard;
+	};
+
+	return {
+		getGameBoard: getGameBoard
+	};
+})();
--- tests/spec/AppSpec.js
+++ tests/spec/AppSpec.js
@@ -6,5 +6,13 @@ describe('Game Logic', function() {
 			expect(Array.isArray(gameBoard)).toBe(true);
 			expect(gameBoard.length).toEqual(3);
 		});
+
+		it('should return 3 array elements, which each contain 3 elements', function() {
+			var gameBoard = game.getGameBoard();
+
+			expect(gameBoard[0].length).toEqual(3);
+			expect(gameBoard[1].length).toEqual(3);
+			expect(gameBoard[2].length).toEqual(3);
+		});
 	});
 });
